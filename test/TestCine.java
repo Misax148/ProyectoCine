f0dcb9810f3bd0224aea7faa227acf701d87e5bc
Axel <axeljas14@gmail.com>
2022-03-14 18:27:56 -0400

    SSSSSSSSS

--- test/TestCine.java
+++ test/TestCine.java
@@ -4,13 +4,13 @@ public class TestCine {
     @Test
 
     public void testCine(){
-        Sala salaUno = new Sala(100, "A1A0");
+        /*Sala salaUno = new Sala(100, "A1A0");
         Sala salaDos = new Sala(100, "B1B0");
         Sala salaTres = new Sala(100,"C1C0" );
         Sala salaCuatro = new Sala(100,"D1D0" );
         Sala salaCinco = new Sala(100, "E1E0");
         Sala salaSeis = new Sala(100, "F1F0");
-        Sala salaSiete = new Sala(100, "G1G0");
+        Sala salaSiete = new Sala(100, "G1G0");*/
     }
 
 
